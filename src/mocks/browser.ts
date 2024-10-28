7c32d139f4e31821b205bdb396f1c1e1a83007a8
wjdsbs <wjdsbss@gmail.com>
2024-10-28 21:29:49 +0900

    feat: userData 모킹 및 api 연동

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,8 +2,10 @@ import { setupWorker } from "msw";
 
 import { projectMockHandler } from "../api/hooks/projectDetail.mock";
 import { teamProgressMockHandler } from "../api/hooks/teamProgress.mock";
+import { userMockHandler } from "../api/hooks/userDetail.mock";
 
 export const worker = setupWorker(
   ...teamProgressMockHandler,
-  ...projectMockHandler
+  ...projectMockHandler,
+  ...userMockHandler
 );
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,10 +1,11 @@
 import { setupServer } from "msw/node";
 
 import { projectMockHandler } from "@/api/hooks/projectDetail.mock";
-
-import { teamProgressMockHandler } from "../api/hooks/teamProgress.mock";
+import { teamProgressMockHandler } from "@/api/hooks/teamProgress.mock";
+import { userMockHandler } from "@/api/hooks/userDetail.mock";
 
 export const server = setupServer(
   ...teamProgressMockHandler,
-  ...projectMockHandler
+  ...projectMockHandler,
+  ...userMockHandler
 );
