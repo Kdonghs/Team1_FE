83b9bff9b4bc6f5295f60dbf4d09168a294e41d0
wjdsbs <wjdsbss@gmail.com>
2024-10-29 23:01:43 +0900

    feat: 모달 애니메이션 적용

--- src/components/common/modal/ProjectSetting/index.tsx
+++ src/components/common/modal/ProjectSetting/index.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Flex,
   ModalBody,
   ModalCloseButton,
   ModalContent,
@@ -10,14 +11,79 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import styled from "@emotion/styled";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 import { DateField } from "../../Fields/dateField";
 import { TextField } from "../../Fields/textField";
 import { ToggleButtonGroup } from "./toggleButtonGroup";
 
+const variants = {
+  entry: (back: boolean) => ({
+    x: back ? -300 : 300,
+    opacity: 0,
+  }),
+  center: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.3 },
+  },
+  exit: (back: boolean) => ({
+    x: back ? 300 : -300,
+    opacity: 0,
+  }),
+};
+
+const renderFooterButtons = (
+  currentPage: number,
+  selectedFeature: string,
+  onClose: () => void,
+  handleNextPage: () => void,
+  handlePreviousPage: () => void
+) => {
+  if (currentPage === 1) {
+    if (selectedFeature === "기본") {
+      return (
+        <StyledButton onClick={onClose} width={"134px"}>
+          저장
+        </StyledButton>
+      );
+    } else {
+      return (
+        <StyledButton onClick={handleNextPage} width={"134px"}>
+          다음
+        </StyledButton>
+      );
+    }
+  } else {
+    return (
+      <Flex justifyContent="space-between" width={"100%"}>
+        <StyledButton onClick={handlePreviousPage} width={"134px"} mr={2}>
+          이전
+        </StyledButton>
+        <StyledButton onClick={onClose} width={"134px"}>
+          저장
+        </StyledButton>
+      </Flex>
+    );
+  }
+};
+
 export const ProjectSettingModal = ({ onClose }: { onClose: () => void }) => {
+  // 기본값 설정
   const [selectedFeature, setSelectedFeature] = useState("기본");
+  const [currentPage, setCurrentPage] = useState(1); // 페이지 상태
+  const [back, setBack] = useState(false);
+
+  const handleNextPage = () => {
+    setCurrentPage(2);
+    setBack(false);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage(1);
+    setBack(true);
+  };
 
   return (
     <>
@@ -27,26 +93,48 @@ export const ProjectSettingModal = ({ onClose }: { onClose: () => void }) => {
           프로젝트 설정
         </ModalHeader>
         <ModalCloseButton m={5} />
-        <ModalBody>
-          <Stack spacing={4} display="flex">
-            <TextField label="프로젝트명" />
-            <DateField label="시작일" />
-            <DateField label="종료일" />
 
-            <ToggleButtonGroup
-              label="기능 설정"
-              options={["기본", "사용자 설정"]}
-              selectedOption={selectedFeature}
-              onChange={setSelectedFeature}
-            />
-          </Stack>
-          <Spacer height={100} />
-        </ModalBody>
+        <AnimatePresence mode="wait" custom={back} initial={false}>
+          <motion.div
+            key={currentPage}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            variants={variants}
+            transition={{ duration: 0.3 }}
+            custom={back}
+          >
+            {currentPage === 1 ? (
+              <ModalBody>
+                <Stack spacing={4}>
+                  <TextField label="프로젝트명" />
+                  <DateField label="시작일" />
+                  <DateField label="종료일" />
+                  <ToggleButtonGroup
+                    label="기능 설정"
+                    options={["기본", "사용자 설정"]}
+                    selectedOption={selectedFeature}
+                    onChange={setSelectedFeature}
+                  />
+                </Stack>
+                <Spacer height={50} />
+              </ModalBody>
+            ) : (
+              <ModalBody>
+                <div>두 번째 페이지</div>
+              </ModalBody>
+            )}
+          </motion.div>
+        </AnimatePresence>
 
         <ModalFooter>
-          <StyledButton onClick={onClose} width={"134px"}>
-            수정
-          </StyledButton>
+          {renderFooterButtons(
+            currentPage,
+            selectedFeature,
+            onClose,
+            handleNextPage,
+            handlePreviousPage
+          )}
         </ModalFooter>
       </StyledModalContent>
     </>
@@ -56,6 +144,7 @@ export const ProjectSettingModal = ({ onClose }: { onClose: () => void }) => {
 const StyledModalContent = styled(ModalContent)`
   border-radius: 16.23px;
   padding: 0.5em;
+  overflow: hidden;
 `;
 
 const StyledButton = styled(Button)`
