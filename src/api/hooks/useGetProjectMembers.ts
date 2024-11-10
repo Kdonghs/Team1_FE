8c8b3b6ed13b4ff34a26309c7d258cec2d7e2a45
wjdsbs <wjdsbss@gmail.com>
2024-11-10 17:43:36 +0900

    feat: 팀원 관리 모달 페이지네이션 구현

--- src/api/hooks/useGetProjectMembers.ts
+++ src/api/hooks/useGetProjectMembers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { getTestToken } from "../../components/features/Project/TokenTest";
 import type { GetMemberListData } from "../generated/data-contracts";
@@ -6,21 +6,20 @@ import { projectApi } from "../projectApi";
 
 export const getProjectMembers = async (
   projectId: number,
-  page: number,
-  size: number,
-  sort: string,
+  query: { page: number; size: number; sort: string },
   role?: string
 ): Promise<GetMemberListData> => {
-  // TODO: any 어떻게해결하지..
+  // TODO: any 해결
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const query: any = {
-    page,
-    size,
-    sort,
+  const queryParams: any = {
+    page: query.page,
+    size: query.size,
+    sort: query.sort,
     role,
   };
+
   const testToken = getTestToken();
-  const response = await projectApi.getMemberList(projectId, query, {
+  const response = await projectApi.getMemberList(projectId, queryParams, {
     headers: {
       Authorization: `Bearer ${testToken}`,
     },
@@ -31,13 +30,25 @@ export const getProjectMembers = async (
 
 export const useGetProjectMembers = (
   projectId: number,
-  page: number,
   size: number,
   sort: string,
   role?: string
 ) =>
-  useQuery<GetMemberListData, Error>({
-    queryKey: ["projectMembers", projectId, page, size, sort, role],
-    queryFn: () => getProjectMembers(projectId, page, size, sort, role),
+  useInfiniteQuery<GetMemberListData, Error>({
+    queryKey: ["projectMembers", projectId, size, sort, role],
+    queryFn: ({ pageParam = 0 }) => {
+      return getProjectMembers(
+        projectId,
+        { page: Number(pageParam), size, sort },
+        role
+      );
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.page !== undefined && lastPage.hasNext) {
+        return lastPage.page + 1;
+      }
+      return undefined;
+    },
+    initialPageParam: 0,
     enabled: !!projectId,
   });
